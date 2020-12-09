135dfbc11043bcc977fd1920fd5dc4dc17eb99f7
aeldeep <eng.ahmed-eldeep@hotmail.com>
2020-12-09 01:29:46 -0500

    2D Array

---
+++ com/eldeep/Java2DArray.java
@@ -0,0 +1,39 @@
+package com.eldeep;
+
+import java.util.Scanner;
+
+public class Java2DArray {
+
+	private static final Scanner scanner = new Scanner(System.in);
+
+	public static void main(String[] args) {
+        int[][] arr = new int[6][6];
+
+        for (int i = 0; i < 6; i++) {
+            String[] arrRowItems = scanner.nextLine().split(" ");
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            for (int j = 0; j < 6; j++) {
+                int arrItem = Integer.parseInt(arrRowItems[j]);
+                arr[i][j] = arrItem;
+            }
+        }
+        int sum =0;
+        int result=0;
+        for (int i=0; i<4;i++)
+        {
+            for (int k =0;k<4;k++)
+            {
+                sum = arr[i][k]+arr[i][k+1]+arr[i][k+2]+arr[i+1][k+1]+arr[i+2][k]+arr[i+2][k+1]+arr[i+2][k+2];
+                if (sum>result)
+                {
+                result=sum;
+
+                }
+            }
+
+        }
+        System.out.println(result);
+        scanner.close();
+    }
+}
